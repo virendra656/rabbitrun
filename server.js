1d9568b143df4c3ae9aedcd7a0b88f27353638e2
virendra656 <android@codesplice.com>
2019-08-06 23:07:28 +0530

    socket io

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const body_parser_1 = require("body-parser");
 const routes = require("./routes/_index");
 const dotenv = require("dotenv");
 const helper_1 = require("./util/helper");
-const PORT = 3000;
+const PORT = 80;
 class Server {
     constructor() {
         dotenv.config();
         this.app = express();
+        let server = require('http').createServer(this.app);
+        let io = require('socket.io')(server);
         this.app.use(cors({
             optionsSuccessStatus: 200
         }));
@@ -28,13 +30,26 @@ class Server {
         this.app.use(boom());
         this.app.use(morgan('combined'));
         this.app.use(expressValidator());
-        this.app.listen(PORT, () => {
+        server.listen(PORT, () => {
             winston.log('info', '--> Server successfully started at port %d', PORT);
         });
         routes.initRoutes(this.app);
         this.app.use(function (err, req, res, next) {
             console.error("err.stack");
             helper_1.renderResponse(res, null, err, null);
+        });
+        this.initSocket(io);
+    }
+    initSocket(io) {
+        io.on('connection', function (client) {
+            console.log('Client connected...');
+            client.on('join', function (data) {
+                console.log(data);
+            });
+            client.on('messages', function (data) {
+                client.emit('broad', data);
+                client.broadcast.emit('broad', data);
+            });
         });
     }
     getApp() {
