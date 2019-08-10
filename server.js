9fdf232c5f6299137b31ce9b9bb2f3ce57aca2c4
virendra656 <android@codesplice.com>
2019-08-10 14:25:43 +0530

    socket

--- server.js
+++ server.js
@@ -10,37 +10,34 @@ const body_parser_1 = require("body-parser");
 const routes = require("./routes/_index");
 const dotenv = require("dotenv");
 const helper_1 = require("./util/helper");
-const PORT = 80;
+const PORT = 3000;
 class Server {
     constructor() {
         dotenv.config();
         this.app = express();
-        let server = require('http').createServer(this.app);
-        let io = require('socket.io')(server);
+        var server = require('http').createServer(this.app);
+        var io = require('socket.io')(server);
+        this.app.use(function (err, req, res, next) {
+            console.error("err.stack");
+        });
         this.app.use(cors({
             optionsSuccessStatus: 200
         }));
         this.app.use(body_parser_1.urlencoded({
             extended: true
         }));
-        this.app.use(function (err, req, res, next) {
-            console.error("err.stack");
-        });
         this.app.use(body_parser_1.json());
         this.app.use(boom());
         this.app.use(morgan('combined'));
         this.app.use(expressValidator());
-        server.listen(PORT, () => {
-            winston.log('info', '--> Server successfully started at port %d', PORT);
-        });
         routes.initRoutes(this.app);
         this.app.use(function (err, req, res, next) {
             console.error("err.stack");
             helper_1.renderResponse(res, null, err, null);
         });
-        this.initSocket(io);
-    }
-    initSocket(io) {
+        server.listen(PORT, () => {
+            winston.log('info', '--> Server successfully started at port %d', PORT);
+        });
         io.on('connection', function (client) {
             console.log('Client connected...');
             client.on('join', function (data) {
@@ -48,9 +45,10 @@ class Server {
             });
             client.on('messages', function (data) {
                 client.emit('broad', data);
-                client.broadcast.emit('broad', data);
             });
         });
+    }
+    initSocket(io) {
     }
     getApp() {
         return this.app;
